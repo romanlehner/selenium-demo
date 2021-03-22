b38c03faf9ca895123fe6c51dab725aaf5403d3c
Roman Lehner <romanlehner@airasia.com>
2021-03-22 21:30:12 +0800

    Add pytest

--- dockerfile
+++ dockerfile
@@ -2,4 +2,4 @@ FROM python:3.7-slim
 COPY requirements.txt .
 RUN pip install -r requirements.txt
 COPY test.py .
-CMD ["python", "test.py" ]
+CMD ["pytest", "test.py" ]
--- test.py
+++ test.py
@@ -1,9 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+import pytest
 
-driver = webdriver.Remote(
-   command_executor='http://selenium-hub:4444/wd/hub',
-   desired_capabilities=DesiredCapabilities.CHROME)
+@pytest.fixture
+def browser(request):
+    driver = webdriver.Remote(
+       command_executor='http://selenium-hub:4444/wd/hub', 
+       desired_capabilities=DesiredCapabilities.CHROME)
+    yield driver
+    driver.quit()
 
-driver.get("http://www.python.org")
-driver.quit()
+def test_browser_interaction(browser):
+   browser.get("http://www.python.org")
+   element = browser.find_element_by_class_name("donate-button")
+   assert element.text == 'Donate'
